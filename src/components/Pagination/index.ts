45a6eee6aad77bd884380a2d0b1651a06b17c389
Dmitry Kudryavtsev <splcell2013@gmail.com>
2023-11-12 21:11:14 +0700

    fix path to selector

--- src/components/Pagination/index.ts
+++ src/components/Pagination/index.ts
@@ -1,5 +1,5 @@
 export {getPage} from './model/selectors/getPage';
 export {getNext} from './model/selectors/getNext';
 export {getPrev} from './model/selectors/getPrev';
-export {getAdaptiveNext} from './model/selectors/getAdaptivenext'
+export {getAdaptiveNext} from './model/selectors/getAdaptiveNext'
 export {getAdaptivePrev} from './model/selectors/getAdaptivePrev'
